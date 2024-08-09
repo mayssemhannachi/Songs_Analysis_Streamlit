419be30bf411eb6e21e4fc052f573cf20d8a71f8
mayssemhannachi <h.mayssem2003@gmail.com>
2024-08-09 19:30:18 +0100

    top albums and top genres done

--- app.py
+++ app.py
@@ -34,8 +34,6 @@ st.set_page_config(page_title='HarmonyHub', page_icon='🔗', layout='wide', ini
 with open("style.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-
-
 # Display the user's profile details
 user = sp.current_user()
 if user['images']:
@@ -53,7 +51,7 @@ st.sidebar.title(f"{user['display_name']}")
 # Number of followers
 st.sidebar.write(f"{user['followers']['total']} followers")
 
- # User's Spotify link and logo in the sidebar
+# User's Spotify link and logo in the sidebar
 spotify_logo_url = "https://upload.wikimedia.org/wikipedia/commons/8/84/Spotify_icon.svg"
 spotify_link = user['external_urls']['spotify']
 
@@ -65,7 +63,7 @@ st.sidebar.markdown(f"""
 """, unsafe_allow_html=True)
 
 # Three columns with different widths
-col1, col2, col3= st.columns([5,2,5])
+col1, col2, col3 = st.columns([5, 2, 5])
 
 # Add the header with the app name
 with col1:
@@ -91,9 +89,12 @@ if current_track:
                 </div>
             </div>
         """, unsafe_allow_html=True)
-        
-col1,col2= st.columns([5,5])  
-   
+
+# Add space between Row A and Row B
+st.markdown("<div style='height: 50px;'></div>", unsafe_allow_html=True)
+
+col1, col2 = st.columns([5, 5])
+
 # Display the user's top artists
 with col1:
     st.header("Top Artists 🎤")
@@ -132,8 +133,8 @@ with col1:
             """,
             unsafe_allow_html=True
         )
-    
-    #see more button
+
+    # see more button
     st.markdown(
         f"""
         <div class="see-all-btn" >
@@ -145,14 +146,14 @@ with col1:
 
 with col2:
     st.header("Top Tracks 🎵")
-    # Get the user's top tracks with the picture 
+    # Get the user's top tracks with the picture
     top_tracks = sp.current_user_top_tracks(limit=20)
     top_tracks_images = []
     for track in top_tracks['items']:
         track_image = track['album']['images'][0]['url'] if track['album']['images'] else None
         top_tracks_images.append(track_image)
 
-    #top 3 tracks
+    # top 3 tracks
     top_3_tracks = top_tracks_images[:3]
     track_images_html = ""
     for track_image in top_3_tracks:
@@ -168,7 +169,7 @@ with col2:
         unsafe_allow_html=True
     )
 
-    # Display the top tracks 
+    # Display the top tracks
     for i, track in enumerate(top_tracks['items'][:9]):
         track_name = track['name']
         track_image_url = track['album']['images'][0]['url'] if track['album']['images'] else None
@@ -188,7 +189,7 @@ with col2:
             unsafe_allow_html=True
         )
 
-    #see more button
+    # see more button
     st.markdown(
         f"""
         <div class="see-all-btn" >
@@ -197,3 +198,120 @@ with col2:
         """,
         unsafe_allow_html=True
     )
+# Add space between Row A and Row B
+st.markdown("<div style='height: 50px;'></div>", unsafe_allow_html=True)
+
+
+col1, col2 = st.columns([5, 5])
+
+
+# Extract user's most listened songs and store in a DataFrame
+top_tracks = sp.current_user_top_tracks(limit=50)
+data = []
+
+for track in top_tracks['items']:
+    album = track['album']
+    album_name = album['name']
+    album_image_url = album['images'][0]['url'] if album['images'] else None
+    artist_name = album['artists'][0]['name']
+    genres = sp.artist(album['artists'][0]['id'])['genres']  # Get genres of the artist
+
+    data.append({
+        'track_name': track['name'],
+        'album_name': album_name,
+        'album_image_url': album_image_url,
+        'artist_name': artist_name,
+        'genres': genres
+    })
+
+df = pd.DataFrame(data)
+
+# Count the number of songs listened to for each album
+album_counts = df['album_name'].value_counts().reset_index()
+album_counts.columns = ['album_name', 'count']
+
+# Merge the counts with the original DataFrame to get album details
+top_albums_df = pd.merge(album_counts, df.drop_duplicates(subset=['album_name']), on='album_name')
+
+with col1:
+    # Display the top albums
+    st.header("Top Albums 📀")
+
+    # Display the top 3 albums' pictures next to each other
+    top_3_albums = top_albums_df.head(3)
+    album_images_html = ""
+    for _, album in top_3_albums.iterrows():
+        album_image_url = album['album_image_url']
+        if album_image_url:
+            album_images_html += f'<img src="{album_image_url}" alt="{album["album_name"]}">'
+
+    st.markdown(
+        f"""
+        <div class="square-gallery" >
+            {album_images_html}
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+
+    # Display the top albums
+    for i, album in top_albums_df.head(9).iterrows():
+        album_name = album['album_name']
+        album_image_url = album['album_image_url']
+        artist_name = album['artist_name']
+
+        st.markdown(
+            f"""
+            <div style="background-color: #14171d; padding: 15px; border-radius: 5px; display: flex; align-items: center; height: 70px;"> 
+                <p style="color: white; opacity: 0.5; margin-right: 20px; font-weight:200; font-size:20px;">{i+1}</p>
+                {'<img src="' + album_image_url + '" width="50" height="50" style="border-radius: 10%; margin-right: 10px;">' if album_image_url else ''}
+                <div>
+                    <p style="color: white; line-height: 30px; margin: 0;">{album_name}</p>
+                    <p style="color: white; opacity: 0.5; margin: 0;">{artist_name}</p>
+                </div>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
+
+    # see more button
+    st.markdown(
+        f"""
+        <div class="see-all-btn" >
+            <a href="https://open.spotify.com/collection/albums" target="_blank">SEE ALL</a>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+
+with col2:
+    # Display the top genres
+    st.header("Top Genres 🎶")
+
+    # Get the top genres from the DataFrame
+    genres = df.explode('genres')['genres'].value_counts().head(5)
+
+    # Display the top genres
+    for i, (genre, count) in enumerate(genres.items()):
+        st.markdown(
+            f"""
+            <div style="background-color: #14171d; padding: 15px; border-radius: 5px; display: flex; align-items: center; gap:20px;height: 70px;"> 
+                <p style="color: white; opacity: 0.5; margin-right: 20px; font-size:20px; margin: 0;">{i+1}</p>
+                <p style="color: white; line-height: 30px; margin: 0; font-weight:800; text-transform: capitalize;">{genre}</p>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
+
+    # see more button
+    st.markdown(
+        f"""
+        <div class="see-all-btn" >
+            <a href="https://open.spotify.com/collection/genres" target="_blank">SEE ALL</a>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+
+# Add space between Row A and Row B
+st.markdown("<div style='height: 30px;'></div>", unsafe_allow_html=True)
